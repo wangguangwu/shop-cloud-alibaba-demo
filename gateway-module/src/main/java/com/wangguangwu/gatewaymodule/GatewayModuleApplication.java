2159ee769b59bde379043cfcb3747d16557c74b8
wangguangwu <wangguangwu1999@gmail.com>
2024-10-11 04:49:29 +0800

    配置网关服务

---
+++ gateway-module/src/main/java/com/wangguangwu/gatewaymodule/GatewayModuleApplication.java
@@ -0,0 +1,13 @@
+package com.wangguangwu.gatewaymodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayModuleApplication.class, args);
+    }
+
+}
