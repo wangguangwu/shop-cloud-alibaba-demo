8b781b721cff4f16307f80d65f663d0af44aacf2
wangguangwu <wangguangwu@pukanghealth.com>
2024-10-10 17:50:28 +0800

    order服务配置nacos config

--- order-module/order-core/src/main/java/com/wangguangwu/ordercore/OrderCoreApplication.java
+++ order-module/order-core/src/main/java/com/wangguangwu/ordercore/OrderCoreApplication.java
@@ -3,6 +3,7 @@ package com.wangguangwu.ordercore;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -13,6 +14,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @MapperScan(basePackages = "com.wangguangwu.ordercore.mapper")
 @EnableTransactionManagement
 @EnableFeignClients
+@RefreshScope
 public class OrderCoreApplication {
 
     public static void main(String[] args) {
